d7c469b7f4a86fe656274f3212ef7704918a8115
alex <alex@one-in-cloud.com>
2020-03-17 18:27:22 +0800

    PyInstaller加密未通过

--- my_installer.py
+++ my_installer.py
@@ -1,16 +1,11 @@
 import os
-
-#pyinstaller -D -n "幸运飞艇-策略2" --clean strategy2_run.py
+import md5util
+import time
 
 if __name__ == "__main__":
     from PyInstaller.__main__ import run
 
-    images_dir = os.path.sep.join(['client','resource','images'])
-
-    # add_files = [
-    #     (images_dir + os.path.sep + '*.png',images_dir)
-    # ]
-
+    key = md5util.Md5Util.get_data_md5({"key": 'www.one-in-cloud.com' + str(time.time())}, ["key"])
 
     opts = [
         '-F',
@@ -20,6 +15,7 @@ if __name__ == "__main__":
         '--add-data=%s;%s' % (os.path.join('client', 'resource', 'sounds', '*.wav'), os.path.join('client', 'resource', 'sounds')),
         '--name=%s' % '幸运飞艇-策略2',
         '--icon=xyft.ico',
+        # '--key=%s' % key,
         '--clean',
         '-y',
         'strategy2_run.py'
